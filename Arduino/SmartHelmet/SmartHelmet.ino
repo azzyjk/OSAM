3f8df671e37219fe79a68a08b99f069b9bb00711
hurukku098 <54278490+hurukku098@users.noreply.github.com>
2019-10-23 17:09:38 +0900

    초음파 센서 추가
    초음파 센서 (HC-SR04) 예제 코드를
    시리얼 통신을 위해 delayMicroseconds(); 함수를 사용하지 않게끔 수정 후 추가

--- Arduino/SmartHelmet/SmartHelmet.ino
+++ Arduino/SmartHelmet/SmartHelmet.ino
@@ -5,9 +5,11 @@
 #define trig 13
 
 SoftwareSerial BT(8, 9); // 8번 TX, 9번 RX
-unsigned long curtime; // 현재 시간
+unsigned long curtimemillis; // 현재 시간(밀리 초 단위)
+unsigned long curtimemicros; // 현재 시간(마이크로 초 단위)
 unsigned long ledtimer; // led 점등 시작 시간
 unsigned long usstimer; // 초음파 센서 트리거 시간
+boolean ussstate = 1;
 const int interval = 3000; // 3초
 
 void setup(){
@@ -17,11 +19,14 @@ void setup(){
   pinMode(Rled,OUTPUT);
   pinMode(trig,OUTPUT);
   pinMode(echo,INPUT);
+  digitalWrite(trig,ussstate);
+  usstimer = micros();
 }
 
 void loop(){
-curtime = millis();
-char a;
+curtimemillis = millis(); //현재 시간 동기화
+curtimemicros = micros(); //현재 시간 동기화
+char val;
   
   if(BT.available()){
     val = BT.read();
@@ -50,4 +55,13 @@ char a;
     digitalWrite(Lled, LOW);
     digitalWrite(Rled, LOW);
   }
+//----------------초음파 센서부 (10us 펄스 폭)------------------
+
+if(curtimemicros-usstimer>=10){
+  ussstate = ~ussstate;
+  digitalWrite(trig,ussstate);
+  usstimer = micros();  
+}
+unsigned long duration;
+
 }
